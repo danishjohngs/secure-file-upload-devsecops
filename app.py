397ecd6e0c2050575bf64dc35caa66ae2c520ade
danishjohngs <danish.sec.dev@gmail.com>
2026-03-03 14:56:47 +0530

    Handled Bandit B104 warning properly

--- app.py
+++ app.py
@@ -57,4 +57,4 @@ def upload_file():
     return f"File '{filename}' uploaded and scanned successfully!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)  # nosec B104
